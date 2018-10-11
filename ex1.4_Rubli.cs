04e0d2b08352a759e88a99f09ce8d3b844f95d54
BurkovIvanV <utka.burkov@yandex.ru>
2018-10-11 13:02:33 +0500

    4 commit

---
+++ ex1.4_Rubli.cs
@@ -0,0 +1,20 @@
+﻿namespace Pluralize
+{
+    public static class PluralizeTask
+    {
+        public static string PluralizeRubles(int count)
+        {
+            // Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+            
+            if (5 <= count % 100 && count % 100 <= 20)
+                return "рублей";
+            else if (2 <= count % 10 && count % 10 <= 4)
+                return "рубля";
+            else if (count % 10 == 1)
+                return "рубль";
+            else
+                return "рублей";
+
+        }
+    }
+}
