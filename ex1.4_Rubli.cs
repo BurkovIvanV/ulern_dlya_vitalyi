f3eccdab9802ea89a61f9bd337d7bd96d4d7a827
BurkovIvanV <utka.burkov@yandex.ru>
2018-10-25 12:36:32 +0500

    review of 4th exersise

--- ex1.4_Rubli.cs
+++ ex1.4_Rubli.cs
@@ -4,17 +4,9 @@
     {
         public static string PluralizeRubles(int count)
         {
-            // Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
-            
-            if (5 <= count % 100 && count % 100 <= 20)
-                return "рублей";
-            else if (2 <= count % 10 && count % 10 <= 4)
+            if (2 <= count % 10 && count % 10 <= 4 && (count % 100 > 14 || count % 100 < 5))
                 return "рубля";
-            else if (count % 10 == 1)
-                return "рубль";
-            else
-                return "рублей";
-
+            return (count % 10 == 1 && count % 100 != 11) ? "рубль" : "рублей";
         }
     }
 }
