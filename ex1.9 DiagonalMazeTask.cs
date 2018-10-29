a7e4c3d53189ecb87ebefbf07c8934985a891bfb
BurkovIvanV <utka.burkov@yandex.ru>
2018-10-30 01:25:34 +0500

    refactoring of 3rd deadline

--- ex1.9 DiagonalMazeTask.cs
+++ ex1.9 DiagonalMazeTask.cs
@@ -4,28 +4,24 @@
     {
         public static void MoveOut(Robot robot, int width, int height)
         {
-            int aspectRatio;
-
+            int aspectRatio = GetAspectRadio(robot, width, height);
             if (width > height)
-            {
-                aspectRatio = (width - 2) / (height - 2);
                 DiagonalGo(robot, aspectRatio, Direction.Right, Direction.Down);
-            }
             else
-            {
-                aspectRatio = (height - 2) / (width - 2);
                 DiagonalGo(robot, aspectRatio, Direction.Down, Direction.Right);
-            }
         }
 
         public static void DiagonalGo(Robot robot, int aspectRatio, Direction directionOne, Direction directionTwo)
         {
-            while (robot.Finished == false)
+            while (!robot.Finished)
             {
                 for (var i = 0; i < aspectRatio; i++)
                     robot.MoveTo(directionOne);
                 if (robot.Finished == false) robot.MoveTo(directionTwo);
             }
         }
+        public static int GetAspectRadio(Robot robot, int width, int height) => 
+            width > height ? (width - 2) / (height - 2) : (height - 2) / (width - 2);
+        
     }
 }
