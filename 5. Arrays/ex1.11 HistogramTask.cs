3b377a5e34e2041bb688d30b5393e4cc080c50ea
BurkovIvanV <utka.burkov@yandex.ru>
2018-10-30 03:04:40 +0500

    4th deadline

---
+++ 5. Arrays/ex1.11 HistogramTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Management.Instrumentation;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var labels = new string[31];
+            for (int i = 0; i < 31; i++)
+            {
+                labels[i] = (i + 1).ToString();
+            }
+            var days = new double[31];
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i].Name == name && names[i].BirthDate.Day != 1)
+                {
+                    days[names[i].BirthDate.Day - 1]++;
+                }
+            }
+            return new HistogramData(string.Format("Рождаемость людей с именем '{0}'", name), labels, days);
+        }
+    }
+}
