01f1ebca22d7be54235389b1e1c21318e38ce1d8
BurkovIvanV <utka.burkov@yandex.ru>
2018-10-30 08:37:39 +0500

    4th dedline

---
+++ 5. Arrays/ex1.12 HeatmapTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static string[] ArrayInit(int length, int minValue)
+        {
+            var fullArray = new string[length];
+            for (int i = 0; i < length; i++)
+                fullArray[i] = (i + minValue).ToString();
+            return fullArray;
+        }
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var label = ArrayInit(30, 2);
+            var months = ArrayInit(12, 1);
+            var heatMap = new double[30, 12];
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i].BirthDate.Day != 1)
+                    heatMap[names[i].BirthDate.Day-2, names[i].BirthDate.Month-1]++;
+            }
+            var result = new HeatmapData("Тепловая карта рождаемости в зависимости от дня и месяца для заданного имени",
+                heatMap, label, months);
+            return result;
+        }
+    }
+}
