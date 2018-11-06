29603a5591f7b8fd5771cdf8d37d10405d03ad3d
BurkovIvanV <utka.burkov@yandex.ru>
2018-11-06 23:20:13 +0500

    5th deadLine

---
+++ ex1.13SentencesParserTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+   public static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            string[] sentenceString;
+            sentenceString = text.Split('.', '!', '?', ':', ';', '(', ')');
+            foreach (var s in sentenceString)
+            {
+                if (ParseSymbols(s).Count != 0)
+                    sentencesList.Add(ParseSymbols(s));
+            }
+            return sentencesList;
+        }
+
+        private static List<string> ParseSymbols(string sentence)
+        {
+            var wordsInSentence = new List<string>();
+            var tempWord = "";
+
+            foreach (var symbol in sentence)
+            {
+                if (char.IsLetter(symbol) || symbol == '\'')
+                {
+                    tempWord += Char.ToLower(symbol);
+                }
+                else if (tempWord != "")
+                    {
+                        wordsInSentence.Add(tempWord);
+                        tempWord = "";
+                    }
+            }
+            if (tempWord.Length != 0)
+                wordsInSentence.Add(tempWord);
+
+            return wordsInSentence;
+        }
+    }
+}
